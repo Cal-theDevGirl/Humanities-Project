4f55d2d630a3ff461f35d23566a89b1c1d394964
Cal-theDevGirl <120177364+Cal-theDevGirl@users.noreply.github.com>
2022-12-12 16:04:23 +0000

    made changes to the navigation menu so that it works and doesnt error out

--- navscript.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#nav-button');
-const menu = document.querySelector('#nav-menu');
-
-button.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
-});
