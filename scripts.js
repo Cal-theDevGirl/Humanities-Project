f2ecde27d41dc0541222f3bcacb4c13153fb575f
Cal-theDevGirl <120177364+Cal-theDevGirl@users.noreply.github.com>
2022-12-12 17:29:52 +0000

    fix

--- scripts.js
+++ scripts.js
@@ -17,3 +17,9 @@ window.onclick = function(event) {
     }
   }
 }
+
+var extern = document.getElementsByTagName("link")[0].import;
+var absatz = extern.getElementsByTagName("div")[0];
+window.addEventListener("load", function() {
+  document.getElementsByTagName("html")[0].replaceChild(extern.getElementsByTagName("body")[0], document.getElementsByTagName("body")[0]);
+}, false);
