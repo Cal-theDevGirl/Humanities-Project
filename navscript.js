35deac91898ccb15e4a46b8cc8de714a9b39a4d2
Cal-theDevGirl <120177364+Cal-theDevGirl@users.noreply.github.com>
2022-12-12 13:33:03 +0000

    Update navscript.js

--- navscript.js
+++ navscript.js
@@ -1,3 +1,6 @@
-function openNav() {
-  document.getElementById("nav").style.width = "250px";
-}
+const button = document.querySelector('#nav-button');
+const menu = document.querySelector('#nav-menu');
+
+button.addEventListener('click', () => {
+  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
